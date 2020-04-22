46d7c9c2e4ee48fd3d5e0be9c5dbddfd81ee0581
kahbernardo <kaiquebernardo02@gmail.com>
2020-04-22 03:36:26 -0300

    meio da aula 38

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,42 +1,64 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
 const Calculator =()=>{
 
+    const initialState = {
+        displayValue:'0',
+        clearDisplay:false,
+        operation:null,
+        values:[0,0],
+        current:0
+    }
+
+    const [State,setState] =useState({
+        displayValue:'0',
+        clearDisplay:false,
+        operation:null,
+        values:[0,0],
+        current:0
+    })
+
+
     const clearMemory=()=> {
-        console.log("limpar")
+        setState(initialState)
     }
 
     const setOperation=(operation)=> {
         console.log(operation)
     }
     const addDigit=(n)=> {
-        console.log(n)
+        if(n==='.'&&State.displayValue.includes('.')){
+            return
+        }
+        if(State.displayValue==='0'||State.clearDisplay){
+            clearMemory()
+        }
     }
 
     return(
             
             <div className="calculator">
-                <Display value="100"/>
-                <Button label="AC" click={()=>clearMemory()} triple/>
-                <Button label="/" click={()=>setOperation()} operation/>
-                <Button label="7" click={()=>addDigit()}/>
-                <Button label="8" click={()=>addDigit()}/>
-                <Button label="9" click={()=>addDigit()}/>
-                <Button label="*" click={()=>setOperation()} operation/>
-                <Button label="4" click={()=>addDigit()}/>
-                <Button label="5" click={()=>addDigit()}/>
-                <Button label="6" click={()=>addDigit()}/>
-                <Button label="-" click={()=>setOperation()} operation/>
-                <Button label="1" click={()=>addDigit()}/>
-                <Button label="2" click={()=>addDigit()}/>
-                <Button label="3" click={()=>addDigit()}/>
-                <Button label="+" click={()=>setOperation()} operation/>
-                <Button label="0" click={()=>addDigit()} double/>
-                <Button label="." click={()=>addDigit()}/>
-                <Button label="=" click={()=>setOperation()} operation/>
+                <Display value={State.displayValue}/>
+                <Button label="AC" click={()=>clearMemory} triple/>
+                <Button label="/" click={()=>setOperation} operation/>
+                <Button label="7" click={()=>addDigit}/>
+                <Button label="8" click={()=>addDigit}/>
+                <Button label="9" click={()=>addDigit}/>
+                <Button label="*" click={()=>setOperation} operation/>
+                <Button label="4" click={()=>addDigit}/>
+                <Button label="5" click={()=>addDigit}/>
+                <Button label="6" click={()=>addDigit}/>
+                <Button label="-" click={()=>setOperation} operation/>
+                <Button label="1" click={()=>addDigit}/>
+                <Button label="2" click={()=>addDigit}/>
+                <Button label="3" click={()=>addDigit}/>
+                <Button label="+" click={()=>setOperation} operation/>
+                <Button label="0" click={()=>addDigit} double/>
+                <Button label="." click={()=>addDigit}/>
+                <Button label="=" click={()=>setOperation} operation/>
             </div>
     )
     
