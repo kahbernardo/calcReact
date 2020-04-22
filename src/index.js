301facbdbaafe21ea853f5a2015648718b1a44d6
kahbernardo <kaiquebernardo02@gmail.com>
2020-04-22 00:03:06 -0300

    aula 34

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Calculator from './main/Calculator'
+import './index.css';
 
 
-const elemento = document.getElementById('root')
+
 
 ReactDOM.render(
-    <div>
-
-    </div>,elemento)
+    <>
+    <h1>Calculadora</h1>
+    <Calculator/>
+    </>
+    , document.getElementById('root'));
---
+++ src/main/Calculator.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Calculator.css'
+
+const Calculator =()=>{
+
+    return(
+            
+            <div className="calculator">
+                
+            </div>
+    )
+    
+
+}
+export default Calculator
